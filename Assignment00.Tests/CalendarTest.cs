83a9f380f176cb1e9d868cd9d56b0e9bde6aa6c2
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-09-04 19:26:41 +0200

    add full tests and implementation for IsLeapYear

--- Assignment00.Tests/CalendarTest.cs
+++ Assignment00.Tests/CalendarTest.cs
@@ -14,4 +14,44 @@ public class CalendarTest
     // Assert
     result.Should().Be(true);
   }
+
+  [Fact]
+  public void IsLeapYear_when_given_1800_returns_false()
+  {
+    Calendar calendar = new Calendar();
+
+    var result = calendar.IsLeapYear(1800);
+
+    result.Should().Be(false);
+  }
+
+  [Fact]
+  public void IsLeapYear_when_given_1700_returns_false()
+  {
+    Calendar calendar = new Calendar();
+
+    var result = calendar.IsLeapYear(1700);
+
+    result.Should().Be(false);
+  }
+
+  [Fact]
+  public void IsLeapYear_when_given_1600_returns_true()
+  {
+    Calendar calendar = new Calendar();
+
+    var result = calendar.IsLeapYear(1600);
+
+    result.Should().Be(true);
+  }
+
+  [Fact]
+  public void IsLeapYear_when_given_2000_returns_true()
+  {
+    Calendar calendar = new Calendar();
+
+    var result = calendar.IsLeapYear(2000);
+
+    result.Should().Be(true);
+  }
 }
--- Assignment00/Calendar.cs
+++ Assignment00/Calendar.cs
@@ -4,8 +4,9 @@ public class Calendar
 {
   public bool IsLeapYear(int year)
   {
-    if (year % 4 == 0) return true;
+    if (year % 4 != 0) return false;
+    if (year % 100 == 0 && year % 400 != 0) return false;
 
-    return false;
+    return true;
   }
 }

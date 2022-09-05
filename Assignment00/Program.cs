c80c1a624544afdbc8e50628165a42eaddbb9b6f
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-09-05 02:22:22 +0200

    implement handling of invalid year in Main

--- Assignment00/Program.cs
+++ Assignment00/Program.cs
@@ -4,15 +4,23 @@ public class Program
 {
   public static void Main(string[] args)
   {
-    Console.Write("Enter a year: ");
-    string? input = Console.ReadLine();
-    input ??= "-1";
-    var year = int.Parse(input);
+    try
+    {
+      Console.Write("Enter a year: ");
+      string? input = Console.ReadLine();
+      input ??= "-1";
+      var year = int.Parse(input);
 
-    Calendar calendar = new Calendar();
-    bool isLeapYear = calendar.IsLeapYear(year);
+      Calendar calendar = new Calendar();
+      bool isLeapYear = calendar.IsLeapYear(year);
 
-    string output = isLeapYear ? "yay" : "nay";
-    Console.WriteLine(output);
+      string output = isLeapYear ? "yay" : "nay";
+      Console.WriteLine(output);
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine("An error occured. Try again.");
+      Console.WriteLine($"Error description: {ex.Message}");
+    }
   }
 }

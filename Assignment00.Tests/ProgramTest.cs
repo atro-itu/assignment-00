9c2a17d7e61be288c274795b04bc2029c780e143
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-09-05 02:25:32 +0200

    add test for handling of malformed input in Main

--- Assignment00.Tests/ProgramTest.cs
+++ Assignment00.Tests/ProgramTest.cs
@@ -64,4 +64,23 @@ public class ProgramTest
       }
     }
   }
+
+  [Fact]
+  public void Main_when_given_malformed_input_prints_error()
+  {
+    using (var mockOutput = new StringWriter())
+    {
+      using (var mockInput = new StringReader("🐛"))
+      {
+        Console.SetOut(mockOutput);
+        Console.SetIn(mockInput);
+
+        var program = Assembly.Load(nameof(Assignment00));
+        program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+
+        var result = mockOutput.ToString();
+        result.Should().Contain("Input string was not in a correct format.");
+      }
+    }
+  }
 }

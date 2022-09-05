19d4d8a0f0dc023a9b4109da38091e989dc394b6
Andreas Trøstrup <andreastrstrup@gmail.com>
2022-09-05 02:04:07 +0200

    implement interface for IsLeapYear

--- Assignment00/Program.cs
+++ Assignment00/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Assignment00;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    Console.Write("Enter a year: ");
+    string? input = Console.ReadLine();
+    input ??= "-1";
+    var year = int.Parse(input);
+
+    Calendar calendar = new Calendar();
+    bool isLeapYear = calendar.IsLeapYear(year);
+
+    string output = isLeapYear ? "yay" : "nay";
+    Console.WriteLine(output);
+  }
+}

30e87c8514d8a782974af81408411fcdc063c0f1
Andreas Trostrup <duckth@A-Desktop.localdomain>
2022-09-07 15:38:20 +0200

    change to notsupportedexception

--- Assignment00/Calendar.cs
+++ Assignment00/Calendar.cs
@@ -4,7 +4,7 @@ public class Calendar
 {
   public bool IsLeapYear(int year)
   {
-    if (year < 1582) throw new ArgumentException("Invalid year. Must be 1582 or greater.");
+    if (year < 1582) throw new NotSupportedException("Invalid year. Must be 1582 or greater.");
     if (year % 4 != 0) return false;
     if (year % 100 == 0 && year % 400 != 0) return false;
 
